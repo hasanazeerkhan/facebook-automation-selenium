65bfbce3a3f4c3ef59886148a3657b19bdbe49a0
Hasan <hasanazeerkhan@gmail.com>
2024-09-25 17:43:28 +0530

    feat: credentials

---
+++ credentials.js
@@ -0,0 +1,13 @@
+const account = {
+    firstName: "MyFirst",
+    lastName: "MyLast",
+    mobileNumber: "+910000000000",
+    password: "MySecretPassword",
+    dayInDob: "10",
+    monthInDob: "Dec",
+    yearInDob: "1999",
+    gender: "Female",
+  };
+  
+  export { account };
+  
